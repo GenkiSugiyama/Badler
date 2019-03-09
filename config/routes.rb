0df9427cb7c2cecc051f11eb177ba9d1e3425e20
GenkiSugiyama <sgym0318marcos@gmail.com>
2019-03-09 23:44:32 +0900

    [Add]チャット機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#top'
+  get 'home/about'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :messages, :only => [:create]
+  resources :rooms, :only => [:create, :show, :index]
 end

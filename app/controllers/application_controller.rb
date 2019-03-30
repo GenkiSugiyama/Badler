caa6a4f97fe865e4b0c952f16f0a181294aeaa4c
Genki Sugiyama <sgym0318marcos@gmail.com>
2019-03-30 02:23:54 +0000

    ユーザー詳細画面等の編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  protected
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    #strong parametersを設定し、usernameを許可
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  end
 end
